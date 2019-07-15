bc184bda0d26ed02ef41ed259fc0145e2a1b887a
Eric <eric.danowski@cengage.com>
2019-07-15 08:47:06 -0400

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,64 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const App: React.FC = () => {
-  const SHAPES = [{ name: "base", coords: [[80, 300], [160, 300]] },
-  { name: "post", coords: [[125, 300], [125, 45]] },
-  { name: "bar", coords: [[125, 45], [300, 45]] }];
-
+  const SHAPES = [
+    { name: "base", shape: "line", coords: [[80, 300], [160, 300]] },
+    { name: "post", shape: "line", coords: [[120, 300], [120, 45]] },
+    { name: "horizontal-bar", shape: "line", coords: [[120, 45], [300, 45]] },
+    { name: "vertical-bar", shape: "line", coords: [[300, 45], [300, 80]] },
+    { name: "head-right", shape: "arc", coords: [[90, 270], [0, 0]] },
+    { name: "head-left", shape: "arc", coords: [[90, -270], [0, 0]] },
+    { name: "body", shape: "line", coords: [[300, 120], [300, 200]] },
+    { name: "left-arm", shape: "line", coords: [[300, 140], [250, 120]] },
+    { name: "right-arm", shape: "line", coords: [[300, 140], [350, 120]] },
+    { name: "left-leg", shape: "line", coords: [[300, 200], [350, 230]] },
+    { name: "right-leg", shape: "line", coords: [[300, 200], [250, 230]] }
+  ];
+  const word = "foobar";
   const canvasRef: React.MutableRefObject<any> = React.useRef(null);
   const [currentShapeIndex, setCurrentShapeIndex] = useState(0);
   const [guessedChars, setGuessedChars] = useState([] as string[]);
+  const [charsLeftToGuess, setCharsLeftToGuess] = useState(word.split(
+    ""
+  ) as string[]);
+
+  const [gameState, setGameState] = useState({ done: false, won: false });
 
   const handleCharClick: any = (e: any) => {
     let selectedChar = e.currentTarget.textContent;
-    if ("foobar".indexOf(selectedChar) === -1) {
+    if (word.indexOf(selectedChar) === -1) {
       drawNextshape();
-    } 
+    }
     let guessed: string[] = [...guessedChars, selectedChar];
-    setGuessedChars(guessed)
+    let charsLeft = charsLeftToGuess.filter(c => c !== selectedChar);
+    setCharsLeftToGuess(charsLeft);
+    setGuessedChars(guessed);
+    setGameState({
+      done: charsLeft.length === 0 || currentShapeIndex === SHAPES.length - 1,
+      won: charsLeft.length === 0
+    });
   };
 
   const drawNextshape: any = () => {
     let index = currentShapeIndex;
     setCurrentShapeIndex(index + 1);
 
-    let {coords: [[sx, sy], [ex, ey]]} = SHAPES[index];
+    let {
+      coords: [[sx, sy], [ex, ey]],
+      shape
+    } = SHAPES[index];
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     ctx.beginPath();
-    ctx.moveTo(sx, sy);
-    ctx.lineTo(ex, ey);
-    ctx.closePath();
+    if (shape === "arc") {
+      ctx.arc(300, 100, 20, (sx * Math.PI) / 180, (sy * Math.PI) / 180, true);
+    } else {
+      ctx.moveTo(sx, sy);
+      ctx.lineTo(ex, ey);
+      ctx.closePath();
+    }
     ctx.stroke();
-
   };
 
   return (
@@ -40,16 +67,27 @@ const App: React.FC = () => {
         <canvas ref={canvasRef} id="hangman" height={320} width={540} />
       </div>
       <div>
-        {"foobar".split("").map(c => (
-          <span> _ </span>
+        {word.split("").map(c => (
+          <span>{guessedChars.includes(c) ? ` ${c} ` : " _ "}</span>
         ))}
       </div>
       <div>
         {"abcdefghijklmnopqrstuvwxyz".split("").map(c => (
-          <button key={c} onClick={e => handleCharClick(e)} disabled={guessedChars.includes(c)}>
+          <button
+            key={c}
+            onClick={e => handleCharClick(e)}
+            disabled={guessedChars.includes(c) || gameState.done}
+          >
             {c}
           </button>
         ))}
+      </div>
+      <div>
+        {gameState.done && gameState.won
+          ? "You Win!"
+          : gameState.done
+          ? "You Lose!"
+          : null}
       </div>
     </div>
   );
